158cd89bf2ff8096a61670663460e043f4314085
Ben Pate <ben@pate.org>
2023-11-30 11:26:42 -0700

    Switching to standard field "loc"

--- zerolog.go
+++ zerolog.go
@@ -17,7 +17,7 @@ func (zerolog Zerolog) Report(err error) {
 	switch typed := err.(type) {
 	case derp.SingleError:
 		buffer, _ := json.MarshalIndent(typed, "", "\t")
-		log.Error().Str("location", typed.Location).Str("message", typed.Message).RawJSON("details", buffer).Send()
+		log.Error().Str("loc", typed.Location).Str("message", typed.Message).RawJSON("details", buffer).Send()
 	default:
 		log.Error().Msg(err.Error())
 	}
